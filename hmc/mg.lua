208457efbe4da0be497aa7d77424287ab4024fd7
James Osborn <osborn@lego.mcs.anl.gov>
2013-04-17 14:36:03 -0500

    added wilson MG

---
+++ hmc/mg.lua
@@ -0,0 +1,23 @@
+local mgIsSet = false
+
+function mgSetup(w)
+  if mgIsSet then return end
+  mgIsSet = true
+  if noMG then return end
+  local kappa = 0.5/mass
+  --local block = {2,2,2,2}
+  local block = {4,4,4,4}
+  local lat = {}
+  for i=1,#latsize do lat[i] = latsize[i]/block[i] end
+  w:mgSet({[-1] = { nlevels=1 }})
+  w:mgSet(
+    {
+      [-2] = { verbose=0 },
+      [-1] = { verbose=1, kappa=kappa, kappanv=kappa, itmax=100, ngcr=8 },
+      [0] = { lattice=lat, nvecs=24,
+	      setup_res=0.4, setup_maxit=100, setup_change_fac=0.5,
+	      npre=0, npost=4, scale=1, cres=0.1, itmax=100, ngcr=8 }
+    }
+  )
+  w:mgSetup()
+end
